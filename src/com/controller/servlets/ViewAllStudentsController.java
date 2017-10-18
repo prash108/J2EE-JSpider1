4bf6ea15844b0542056a946d60c16cd4708ed25e
prash108 <kumar108prashant@gmail.com>
2017-10-18 15:59:11 +0530

    controller mvc modified

--- src/com/controller/servlets/ViewAllStudentsController.java
+++ src/com/controller/servlets/ViewAllStudentsController.java
@@ -19,7 +19,7 @@ public class ViewAllStudentsController extends HttpServlet
 {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+//
 		//int regno=Integer.parseInt(req.getParameter("regno"));
 		//String password=req.getParameter("passwd");
 		//String isChecked=req.getParameter("rememberme");
